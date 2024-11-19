197fc435cb932f62cd46ec6af436e2be82bb4dd0
jakob.derzapf <Jakob.D.Baron@outlook.de>
2024-11-19 16:17:30 +0100

    Projektdateien hinzufügen.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace WPFDB_ZooManager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            
+
+            InitializeComponent();
+            //string connectionString = ConfigurationManager.ConnectionStrings["".];
+            //sqlConnection = new SQLConnection(ConnectionString);
+        }
+
+
+        public void ShowZoos()
+        {
+            string query = "select * from Zoo";
+            //SQLDataAdapter sQLDataAdapter = new SQLDataAdapter(query, sqlConnection);
+
+        }
+
+    }
+}
